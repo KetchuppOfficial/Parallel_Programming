d62bc46ec27121fb40baac38b5f5994e41510bb2
KetchuppOfficial <dolgov04@list.ru>
2024-04-27 16:26:35 +0300

    Sequential solver of transport equation implemented

---
+++ 01-transport-equation/include/grid.hpp
@@ -0,0 +1,33 @@
+#ifndef INCLUDE_GRID_HPP
+#define INCLUDE_GRID_HPP
+
+#include <cstddef>
+#include <vector>
+
+namespace parallel
+{
+
+class Grid final
+{
+public:
+
+    Grid(std::size_t N_t, std::size_t N_x)
+        : grid_(N_t * N_x), N_t_{N_t}, N_x_{N_x} {}
+
+    std::size_t t_size() const noexcept { return N_t_; }
+    std::size_t x_size() const noexcept { return N_x_; }
+
+    double operator[](std::size_t k, std::size_t m) const { return grid_.data()[k * N_x_ + m]; }
+
+    double &operator[](std::size_t k, std::size_t m) { return grid_.data()[k * N_x_ + m]; }
+
+private:
+
+    std::vector<double> grid_;
+    const std::size_t N_t_;
+    const std::size_t N_x_;
+};
+
+} // namespace parallel
+
+#endif // INCLUDE_GRID_HPP
