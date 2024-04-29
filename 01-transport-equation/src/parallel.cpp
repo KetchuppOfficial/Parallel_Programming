61dc0bebe4dfb8040fd07a1878b74855cfaab3b5
KetchuppOfficial <dolgov04@list.ru>
2024-04-29 12:53:36 +0300

    boost::program_options used in parallel version of the program

--- 01-transport-equation/src/parallel.cpp
+++ 01-transport-equation/src/parallel.cpp
@@ -5,18 +5,58 @@
 
 #include <boost/mpi/environment.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/program_options.hpp>
 
 #include "parallel_solver.hpp"
 #include "solution_visualization.hpp"
 
-int main()
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[])
 {
+    #ifndef BOOST_MPI_HAS_NOARG_INITIALIZATION
+    static_assert(false, "Program requires command line arguments for boost::program_options");
+    #endif
+
+    po::options_description desc{"Allowed options"};
+
+    desc.add_options()
+        ("help", "Produce help message")
+        ("multiplier", po::value<std::size_t>(), "The number of processes times multiplier equals "
+                                                 "the number of segments on X axis of the grid. "
+                                                 "The number of segment on T axis of the grid is 2 "
+                                                 "times greater")
+        ("plot", "plot solution");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
     boost::mpi::environment env;
     boost::mpi::communicator world;
 
-    auto start = std::chrono::high_resolution_clock::now();
+    if (vm.count("help"))
+    {
+        if (world.rank() == 0)
+            std::cout << desc << std::endl;
 
-    std::size_t N_x = world.size() * 100;
+        return 0;
+    }
+
+    std::size_t multiplier;
+    if (vm.count("multiplier"))
+        multiplier = vm["multiplier"].as<std::size_t>();
+    else
+    {
+        if (world.rank() == 0)
+            std::cout << "Multiplier is not set. Abort" << std::endl;
+
+        return 0;
+    }
+
+    std::size_t N_x = world.size() * multiplier;
+
+    auto start = std::chrono::high_resolution_clock::now();
 
     parallel::Transport_Equation_PSolver solution
     {
@@ -35,9 +75,9 @@ int main()
         using mcs = std::chrono::microseconds;
         std::cout << "Parallel solving on " << world.size() << " nodes took: "
                   << std::chrono::duration_cast<mcs>(stop - start).count() << " mcs" << std::endl;
-        #if 0
-        plot_solution(solution, "x + t");
-        #endif
+
+        if (vm.count("plot"))
+            plot_solution(solution, "x + t");
     }
 
     return 0;
