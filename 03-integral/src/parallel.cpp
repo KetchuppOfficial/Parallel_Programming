e9e122d0321daba51d021fb826a1197dd33873fd
KetchuppOfficial <dolgov04@list.ru>
2024-05-11 17:56:42 +0300

    boost::program_options is used

--- 03-integral/src/parallel.cpp
+++ 03-integral/src/parallel.cpp
@@ -2,15 +2,73 @@
 #include <cmath>
 #include <chrono>
 #include <format>
+#include <tuple>
+#include <optional>
+
+#include <boost/program_options.hpp>
 
 #include "parallel_integrator.hpp"
 
-int main()
+static std::optional<std::tuple<double, double, std::size_t>> get_options(int argc, char *argv[])
 {
-    parallel::Parallel_Integrator integrator{[](double x){ return std::sin(1 / x); }, 1e-8};
+    namespace po = boost::program_options;
 
-    constexpr double a = 0.0001, b = 0.1;
-    constexpr std::size_t n_threads = 10;
+    po::options_description desc{"Allowed options"};
+    desc.add_options()
+        ("help", "Produce help message")
+        ("from", po::value<double>(), "Set the lower limit of integration")
+        ("to", po::value<double>(), "Set the upper limit of integration")
+        ("n-threads", po::value<std::size_t>(), "Set the number of threads to run the program on");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return std::nullopt;
+    }
+
+    double a;
+    if (vm.count("from"))
+        a = vm["from"].as<double>();
+    else
+    {
+        std::cout << "The lower limit of integration is not set. Abort" << std::endl;
+        return std::nullopt;
+    }
+
+    double b;
+    if (vm.count("to"))
+        b = vm["to"].as<double>();
+    else
+    {
+        std::cout << "The upper limit of integration is not set. Abort" << std::endl;
+        return std::nullopt;
+    }
+
+    std::size_t n_threads;
+    if (vm.count("n-threads"))
+        n_threads = vm["n-threads"].as<std::size_t>();
+    else
+    {
+        std::cout << "The number of threads is not set. Abort" << std::endl;
+        return std::nullopt;
+    }
+
+    return std::tuple{a, b, n_threads};
+}
+
+int main(int argc, char *argv[])
+{
+    auto opts = get_options(argc, argv);
+    if (!opts.has_value())
+        return 0;
+
+    auto [a, b, n_threads] = opts.value();
+
+    parallel::Parallel_Integrator integrator{[](double x){ return std::sin(1 / x); }, 1e-8};
 
     auto start = std::chrono::high_resolution_clock::now();
     double I = integrator.integrate(a, b, n_threads);
--- 03-integral/src/sequential.cpp
+++ 03-integral/src/sequential.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <utility>
+#include <optional>
+
+#include <boost/program_options.hpp>
 
 #include "sequential_integrator.hpp"
 
-int main()
+static std::optional<std::pair<double, double>> get_options(int argc, char *argv[])
 {
-    parallel::Sequential_Integrator integrator{[](double x){ return std::sin(1 / x); }, 1e-8};
+    namespace po = boost::program_options;
 
-    constexpr double a = 0.0001, b = 0.1;
+    po::options_description desc{"Allowed options"};
+    desc.add_options()
+        ("help", "Produce help message")
+        ("from", po::value<double>(), "Set the lower limit of integration")
+        ("to", po::value<double>(), "Set the upper limit of integration");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return std::nullopt;
+    }
+
+    double a;
+    if (vm.count("from"))
+        a = vm["from"].as<double>();
+    else
+    {
+        std::cout << "The lower limit of integration is not set. Abort" << std::endl;
+        return std::nullopt;
+    }
+
+    double b;
+    if (vm.count("to"))
+        b = vm["to"].as<double>();
+    else
+    {
+        std::cout << "The upper limit of integration is not set. Abort" << std::endl;
+        return std::nullopt;
+    }
+
+    return std::pair{a, b};
+}
+
+int main(int argc, char *argv[])
+{
+    auto opts = get_options(argc, argv);
+    if (!opts.has_value())
+        return 0;
+
+    auto [a, b] = opts.value();
+
+    parallel::Sequential_Integrator integrator{[](double x){ return std::sin(1 / x); }, 1e-8};
 
     auto start = std::chrono::high_resolution_clock::now();
     double I = integrator.integrate(a, b);
