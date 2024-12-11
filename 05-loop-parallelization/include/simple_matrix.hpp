15038bed5f6efe1a21f03baa0189a29f32169be9
KetchuppOfficial <dolgov04@list.ru>
2024-12-11 20:31:14 +0300

    add method size() and a ctor from std::vector and dimensions

--- 05-loop-parallelization/include/simple_matrix.hpp
+++ 05-loop-parallelization/include/simple_matrix.hpp
@@ -2,7 +2,9 @@
 #define INCLUDE_SIMPLE_MATRIX_HPP
 
 #include <cstddef>
+#include <stdexcept>
 #include <vector>
+#include <utility>
 
 namespace parallel
 {
@@ -14,6 +16,13 @@ public:
     SimpleMatrix(std::size_t n_rows, std::size_t n_cols)
         : data_(n_rows * n_cols), n_rows_{n_rows}, n_cols_{n_cols} {}
 
+    SimpleMatrix(std::vector<double> data, std::size_t n_rows, std::size_t n_cols)
+        : data_{std::move(data)}, n_rows_{n_rows}, n_cols_{n_cols}
+    {
+        if (size() != n_rows * n_cols)
+            throw std::invalid_argument{"size of data doesn't match dimensions"};
+    }
+
     template<typename Self>
     auto &&operator[](this Self &&self, std::size_t i, std::size_t j) noexcept
     {
@@ -22,6 +31,8 @@ public:
 
     std::size_t n_rows() const noexcept { return n_rows_; }
     std::size_t n_cols() const noexcept { return n_cols_; }
+
+    std::size_t size() const noexcept { return data_.size(); }
 
 private:
 
